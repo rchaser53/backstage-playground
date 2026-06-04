b9ce6c99cfb137a690261f1b97709f5e3f11ca31
rchaser53 <tayoshizawa29@gmail.com>
2026-06-04 22:45:50 +0900

    try to add snyk catalog

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,7 +1,8 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
+import snykPlugin from 'backstage-plugin-snyk/alpha';
 import { navModule } from './modules/nav';
 
 export default createApp({
-  features: [catalogPlugin, navModule],
+  features: [catalogPlugin, snykPlugin, navModule],
 });
