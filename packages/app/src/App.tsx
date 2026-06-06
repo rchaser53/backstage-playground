f7837b41994da720ca1e7ee4dd05396412c05b43
rchaser53 <tayoshizawa29@gmail.com>
2026-06-06 21:43:37 +0900

    try to show k8s info

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,8 +1,9 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
+import kubernetesPlugin from '@backstage/plugin-kubernetes/alpha';
 import snykPlugin from 'backstage-plugin-snyk/alpha';
 import { navModule } from './modules/nav';
 
 export default createApp({
-  features: [catalogPlugin, snykPlugin, navModule],
+  features: [catalogPlugin, kubernetesPlugin, snykPlugin, navModule],
 });
--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -9,8 +9,15 @@
 import { createBackend } from '@backstage/backend-defaults';
 import {
   coreServices,
+  createBackendModule,
   createBackendPlugin,
 } from '@backstage/backend-plugin-api';
+import { kubernetesFetcherExtensionPoint } from '@backstage/plugin-kubernetes-node';
+import type {
+  CustomResource,
+  KubernetesFetcher,
+  ObjectFetchParams,
+} from '@backstage/plugin-kubernetes-node';
 import path from 'path';
 import express from 'express';
 
@@ -66,6 +73,44 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 
 // kubernetes plugin
 backend.add(import('@backstage/plugin-kubernetes-backend'));
+backend.add(
+  createBackendModule({
+    pluginId: 'kubernetes',
+    moduleId: 'core-api-custom-resources',
+    register(env) {
+      env.registerInit({
+        deps: {
+          kubernetesFetcher: kubernetesFetcherExtensionPoint,
+        },
+        async init({ kubernetesFetcher }) {
+          kubernetesFetcher.addFetcher(async ({ getDefault }) => {
+            const defaultFetcher = await getDefault();
+
+            return {
+              fetchObjectsForService(params: ObjectFetchParams) {
+                return defaultFetcher.fetchObjectsForService({
+                  ...params,
+                  customResources: params.customResources.map(resource =>
+                    resource.group === 'core'
+                      ? ({ ...resource, group: '' } as CustomResource)
+                      : resource,
+                  ),
+                });
+              },
+              fetchPodMetricsByNamespaces(
+                ...args: Parameters<
+                  KubernetesFetcher['fetchPodMetricsByNamespaces']
+                >
+              ) {
+                return defaultFetcher.fetchPodMetricsByNamespaces(...args);
+              },
+            };
+          });
+        },
+      });
+    },
+  }),
+);
 
 // notifications and signals plugins
 backend.add(import('@backstage/plugin-notifications-backend'));
